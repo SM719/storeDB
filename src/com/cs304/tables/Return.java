2d6e2a32a14e9d2f58c3fca1c654461cbd2650ef
Kyle Lee <kylelee6@gmail.com>
2013-08-06 15:53:03 -0700

    Purchase with the auto generate IDs
    insertion gets screwy sometimes, no idea why, might be just on my end,
    return not done yet

---
+++ src/com/cs304/tables/Return.java
@@ -0,0 +1,10 @@
+package com.cs304.tables;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Date;
+import java.util.GregorianCalendar;
