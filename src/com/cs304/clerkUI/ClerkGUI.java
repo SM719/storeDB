20937f44d9cb1428b8edb122fbdcb4b95cadf79e
Kyle Lee <kylelee6@gmail.com>
2013-08-08 00:19:51 -0700

    Fixed, (mainframe needs to propagate down)
    Fixes some really unexpected errors, doesnt even make sense

--- src/com/cs304/clerkUI/ClerkGUI.java
+++ src/com/cs304/clerkUI/ClerkGUI.java
@@ -11,23 +11,27 @@ import java.sql.Connection;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ClerkGUI {
 
-	// UNCOMMENT LATER
+
+public class ClerkGUI  {
+
+	//UNCOMMENT LATER
 	Connection connection;
-
-	private JFrame Frame;
-
+	
+	private JFrame Frame,mainframe;
+	
+	
 	private JButton processPurchase;
 	private JButton processReturn;
-
-	// constructor
-	public ClerkGUI(Connection con) {
-
-		connection = con;
-
+	
+	// constructor 
+	public ClerkGUI(Connection connect, JFrame fr) {
+		mainframe = fr;
+		connection = connect;
 		Frame = new JFrame("Clerk Operations");
 		Frame.setVisible(true);
 		Frame.setSize(400, 300);
@@ -37,16 +41,19 @@ public class ClerkGUI {
 
 		GridBagLayout gb = new GridBagLayout();
 		GridBagConstraints g = new GridBagConstraints();
-
+		
 		JPanel panel = new JPanel();
-
+		
 		processPurchase = new JButton("Process a purchase of items");
 		processReturn = new JButton("Process a return of items");
-
+		
 		/*
-		 * g.gridx = 0; g.gridy = 0; g.insets = new Insets(10, 29, 10, 10);
-		 * gb.setConstraints(text, g); panel.add(text);
-		 */
+		g.gridx = 0;
+		g.gridy = 0;
+		g.insets = new Insets(10, 29, 10, 10);
+		gb.setConstraints(text, g);
+		panel.add(text);
+		*/
 
 		g.gridx = 0;
 		g.gridy = 1;
@@ -61,27 +68,38 @@ public class ClerkGUI {
 		g.insets = new Insets(10, 10, 10, 10);
 		gb.setConstraints(processReturn, g);
 		panel.add(processReturn);
-
+		
 		panel.setLayout(gb);
-
+		
 		theHandler Handler = new theHandler();
 		processPurchase.addActionListener(Handler);
-		processPurchase.addActionListener(Handler);
-
+		processReturn.addActionListener(Handler);
+		
 		Frame.add(panel);
 	}
-
+	
 	// takes one method, whenever an event occurs
-	private class theHandler implements ActionListener {
+	private class theHandler implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent event) {
-
-			// String string = new String();
-
-			// JOptionPane.showMessageDialog(null, string);
+			
+			if (event.getSource() == processPurchase)
+			{
+				Frame.setVisible(false);
+				
+			} else if (event.getSource() == processReturn)
+			{
+				Frame.setVisible(false);
+				returnUI returnp = new returnUI (connection,mainframe);
+			}
+			
+			
+			
+			//JOptionPane.showMessageDialog(null, string);
 		}
-
+		
+		
 	}
-
+	
 }
