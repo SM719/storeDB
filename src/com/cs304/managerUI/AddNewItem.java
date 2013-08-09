6b68cd44b04fe62594d1f97625c49536538db295
Sukhi <sm_719@hotmail.com>
2013-08-08 18:09:35 -0700

    changed textfields for new item ui

--- src/com/cs304/managerUI/AddNewItem.java
+++ src/com/cs304/managerUI/AddNewItem.java
@@ -45,19 +45,20 @@ public class AddNewItem {
 
 		JPanel panel = new JPanel();
 
-		price = new JTextField("Enter price:");
-		title = new JTextField("Enter title:");
-		type = new JTextField("Enter item type:");
-		category = new JTextField("Enter the category:");
-		company = new JTextField("Enter the company:");
-		year = new JTextField("Enter the year:");
+		price = new JTextField("Enter item price:");
+		title = new JTextField("Enter item title:");
+		type = new JTextField("Enter item type cd or dvd:");
+		category = new JTextField("Enter item category:");
+		company = new JTextField("Enter item company:");
+		year = new JTextField("Enter item year:");
 		add = new JButton("Add Item");
 		cancel = new JButton("Cancel");
-		songs = new JTextField("Enter songs (seperate each song by comma)");
-		singers = new JTextField("Enter singers (seperate each song by comma)");
+		songs = new JTextField("Enter item songs (seperate each song by comma)");
+		singers = new JTextField(
+				"Enter item singers (seperate each song by comma)");
 
 		g.gridx = 0;
-		g.gridy = 1;
+		g.gridy = 8;
 		g.gridwidth = 10;
 		g.fill = GridBagConstraints.HORIZONTAL;
 		g.insets = new Insets(10, 10, 10, 10);
@@ -65,43 +66,43 @@ public class AddNewItem {
 		panel.add(songs);
 
 		g.gridx = 0;
-		g.gridy = 2;
+		g.gridy = 1;
 		g.insets = new Insets(10, 10, 10, 10);
 		gb.setConstraints(price, g);
 		panel.add(price);
 
 		g.gridx = 0;
-		g.gridy = 3;
+		g.gridy = 7;
 		g.insets = new Insets(10, 10, 10, 10);
 		gb.setConstraints(singers, g);
 		panel.add(singers);
 
 		g.gridx = 0;
-		g.gridy = 4;
+		g.gridy = 2;
 		g.insets = new Insets(10, 10, 10, 10);
 		gb.setConstraints(title, g);
 		panel.add(title);
 
 		g.gridx = 0;
-		g.gridy = 5;
+		g.gridy = 3;
 		g.insets = new Insets(10, 10, 10, 10);
 		gb.setConstraints(type, g);
 		panel.add(type);
 
 		g.gridx = 0;
-		g.gridy = 6;
+		g.gridy = 4;
 		g.insets = new Insets(10, 10, 10, 10);
 		gb.setConstraints(category, g);
 		panel.add(category);
 
 		g.gridx = 0;
-		g.gridy = 7;
+		g.gridy = 5;
 		g.insets = new Insets(10, 10, 10, 10);
 		gb.setConstraints(company, g);
 		panel.add(company);
 
 		g.gridx = 0;
-		g.gridy = 8;
+		g.gridy = 6;
 		g.insets = new Insets(10, 10, 10, 10);
 		gb.setConstraints(year, g);
 		panel.add(year);
